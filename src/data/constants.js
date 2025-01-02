031421d6c849f73ce9cb504069d914d6aad1ed6a
Abhishek Kumar Rai <75179538+abhishek00210@users.noreply.github.com>
2025-01-02 19:06:50 +0530

    first-commit

---
+++ src/data/constants.js
@@ -0,0 +1,289 @@
+
+export const Bio = {
+  name: "Abhishek Kumar Rai ",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I'm a highly motivated and versatile individual with a strong aptitude for problem-solving and a deep commitment to continuous development. I enjoy tackling complex challenges, analyzing data, and developing effective solutions. I'm a firm believer in lifelong learning and actively pursue opportunities to expand my knowledge and skillset.",
+  github: "https://github.com/abhishek00210",
+  gmail: "saintlucifer00210@gmail.com",
+  resume:
+    "https://drive.google.com/file/d/1oAXGdoiH72bSij9WiH4_UUVTWd-WJ-dz/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/abhishek-kumar-rai-bb3905221/",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: " https://raw.githubusercontent.com/anshita005/My-Portfolio/main/tailwind-css-logo-png_seeklogo-354675.png",
+      },
+     
+   
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          ""
+          },
+  
+
+   
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+   
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "C++",
+        image:
+          "https://raw.githubusercontent.com/anshita005/My-Portfolio/main/c-logo-png_seeklogo-272951.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://www.svgrepo.com/show/452091/python.svg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "	https://w7.pngwing.com/pngs/80/196/png-transparent-netlify-hd-logo.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name:"vercel",
+        image:"https://www.svgrepo.com/show/354513/vercel-icon.svg",
+
+      },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.koach.live/assets/koach_logo-DnN6mny1.png",
+    role: "Software Developer ",
+    company: "Koach Off",
+    date: "Jan 2023 - Apr 2023",
+    desc: " Developed dedicated sections for coaches and students to facilitate seamless hiring interactions, resulting in a 25% increase in user engagement. Implemented and tested over 15 components, enhancing the application's functionality by 50%. Leveraged advanced React.js features such as useState, useEffect, useContext, and custom hooks to optimize state management and improve component reusability, achieving a 30% increase in code efficiency.",
+       skills: [
+      "ReactJS",
+      "Redux",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "API Integration",
+      "Bootstrap"
+    ],
+    doc: "https://media.licdn.com/dms/document/media/v2/D4D2DAQGFZdHpJTtpSg/profile-treasury-document-pdf-analyzed/profile-treasury-document-pdf-analyzed/0/1683392643010?e=1736380800&v=beta&t=ezX6pPRTh6QfY-a4gCkpaORPPln5G080DS4oA0anCJI",
+  },
+
+];
+export const achievement = [
+  {
+    id: 0,
+    img: "https://img.icons8.com/?size=100&id=wDGo581Ea5Nf&format=png&color=000000",
+    role: "Ranked 1877th ",
+    company: "among 23,596 participants ",
+    date: "Biweekly Contest 145",
+    plateform:"LeetCode",
+    desc: "  ",
+       skills: [
+      
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  },
+  {
+    id: 1,
+    img: "	https://cdn.codechef.com/images/cc-logo.svg",
+    role: "Ranked 53th ",
+    company: "among 14,702 participants ",
+    date: "Starters 163",
+    plateform:"CodeChef",
+    desc: "  ",
+       skills: [
+      
+    ],
+    doc: "" },
+  {
+    id: 2,
+    img: "https://media.geeksforgeeks.org/gfg-gg-logo.svg",
+    role: "Ranked 178th ",
+    company: "among 1,961 participants",
+    date: "GfG Weekly - 182",
+    plateform:"GeekforGeeks",
+    desc: "  ",
+       skills: [
+      
+    ],
+    doc: ""  },
+  
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcumx9TOCJeHnr_ATGUCqb50MOk1SkhexQKumVxxLESztjAvmqvXI5eDCsu53ejTazQU4&usqp=CAU",
+    school: "Galgotias Univeristy",
+    date: "Oct 2021 - June 2025",
+    grade: " 7.89 CGPA",
+    desc: "I am Abhishek Kumar Rai, a dedicated and passionate Computer Science and Engineering student at Galgotias University, set to graduate in July 2025. I have a strong foundation in full-stack development, specializing in technologies like React.js, Node.js, Express.js, and MongoDB, along with proficiency in programming languages such as JavaScript, C++, Python, and SQL. My practical experience includes developing impactful web applications like an EdTech platform and a hostel management system, where I implemented features such as real-time tracking, secure authentication, and enhanced user experiences. I thrive on solving complex problems, as evidenced by my competitive programming achievements, where I rank among the top performers in contests like LeetCode, CodeChef, and Meta HackerCup. Beyond academics, I am an innovator who loves participating in hackathons and coding events, always seeking opportunities to apply my skills to real-world challenges. I am ready to bring my technical expertise, problem-solving mindset, and passion for development to contribute effectively to dynamic teams and impactful projects.", 
+    degree: "Bachelor of Technology - BTech, Computer Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Study Notion",
+    description:
+      "Enabled instructor course publication for sale, increasing total course offerings by 15%. Optimized course purchasing process for students, enhancing transaction efficiency by 30%.. Enhanced user experience by 25% with multiple user types and distinct functionalities. Improved system reliability by 30% with authentication, course creation, purchasing, and content access. Supported CRUD operations, authentication, and image uploading capabilities, boosting operational efficiency by 35%",
+    image:
+      "https://i.ibb.co/jDn4Z5s/studynotion.png",
+    tags: [
+      "MongoDb",
+      "Express.Js",
+      "React.js",
+      "Node.Js",
+      "Tailwind CSS",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/anshita005/Job-Portal",
+    webapp: "https://github.com/anshita005",
+  },
+  {
+    id: 12,
+    title: "Hostel Management Application",
+    description:
+      "Developed a MERN stack solution for efficient student attendance management. Implemented real-time attendance tracking, analytics, and CSV downloads for enhanced user experience. Refined security measures, admin controls and data deletion functionality.",
+    image:
+      "https://i.ibb.co/bJyqgNm/Screenshot-2024-12-29-at-9-31-20-PM.png",
+    tags: [
+    "MongoDb",
+      "Express.Js",
+      "React.js",
+      "Node.Js",
+      "CSS",
+     
+    ],
+    category: "web app",
+    github: "https://github.com/anshita005/My-Portfolio",
+    webapp: "https://github.com/anshita005",
+  },
+  {
+    id: 9,
+    title: "Banking System",
+    description:
+      " Designed and developed a banking system using C++. Implemented functionalities for account creation, deposit/withdrawal, and balance inquiries. Emphasized modular code design and secure transaction handling. Successfully designed a scalable and secure banking system from scratch. Delivered features like real-time balance tracking, secure transactions, and user-friendly interfaces. Gained experience in software design principles and practical implementation of OOP concepts. ",
+      image:
+      "https://guiprojects.com/wp-content/uploads/2024/02/bi-7.png",
+    tags: [
+     "C++",
+     "Opps",
+  
+    ],
+    category: "web app",
+    github: "https://github.com/anshita005/SpendWise",
+    webapp: "https://github.com/anshita005/SpendWise",
+  },
+  {
+    id: 0,
+    title: "Sorting-Visualizer",
+    description:
+      "Created a web application to visualize the working of 5 sorting algorithms such as insertion sort, merge sort, quick sort, and bubble sort. Improved learning experience by 50% with adjustable visualization speeds ranging from 0.5x to 4x.",
+ image:
+      "https://i.ibb.co/3W8tTwG/Screenshot-2024-12-29-at-9-42-56-PM.png",
+    tags: ["React Js", "Javascript", "GeminiAPI", "CSS3"],
+    category: "web app",
+    github: "https://github.com/anshita005/Gemini-Clone-Project",
+    webapp: "https://github.com/anshita005/Gemini-Clone-Project",
+  },
+  {
+    id: 0,
+    title: "Chat App",
+    description:
+      "Built a chat application enabling instant messaging between users, using React and FireBase. Incorporated features like user authentication, private/group chat, and message persistence. Solved real-time data synchronization issues using WebSocket connections.",
+    image:
+      "https://i.ibb.co/FwzwPbt/Screenshot-2024-12-29-at-9-46-19-PM.png",
+    tags: ["React Js", "SCSS"],
+    category: "web app",
+    github: "https://github.com/anshita005/Gemini-Clone-Project",
+    webapp: "https://github.com/anshita005/Gemini-Clone-Project",
+  },
+  
+];
+
